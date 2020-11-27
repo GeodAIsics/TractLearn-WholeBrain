f15834a3f722845ff71ca41d7daba451f2545c43
Renard <felixrenard@gmail.com>
2020-11-27 13:47:52 +0100

    Update 2_Register_fod2template_nomask.py
    automatically create folder transformed_template and warped_template if necessary

--- 2_Register_fod2template_nomask.py
+++ 2_Register_fod2template_nomask.py
@@ -2,10 +2,24 @@
 #The working directory being the directory where all your individuals are stored + two folders named warped_template and transformed template respectively + the template named template_FOD.nii.gz
 #The file name corresponds to the common pattern name of each individual FOD map, for example *_wmfod_norm_average.mif
 
+import os
 from glob import glob
 import numpy as np
 from subprocess import call
 import sys
+
+if not os.path.isdir("transformed_template"):
+	print("Create folder transformed_template")
+	os.mkdir("transformed_template")
+else:
+	print("Folder transformed_template already exists!")
+
+if not os.path.isdir("warped_template"):
+        print("Create folder warped_template")
+        os.mkdir("warped_template")
+else:
+        print("Folder warped_template already exists!")
+
 
 rep =  sys.argv[-2]
 name_fod = sys.argv[-1]
