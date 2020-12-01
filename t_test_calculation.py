d8e1edda463d554b1c8ffcbd4d66dbe3e13cd674
Renard <felixrenard@gmail.com>
2020-12-01 20:45:29 +0100

    Update t_test_calculation.py
    Modify rep folder by stat folder

--- t_test_calculation.py
+++ t_test_calculation.py
@@ -16,9 +16,6 @@ from sklearn.metrics import pairwise_distances
 from sklearn.model_selection import LeaveOneOut
 
 
-#Get the folder directory
-rep = sys.argv[-4]
-
 Track_name = ['AF_left', 'AF_right', 'ATR_left', 'ATR_right', 'CC_1', 'CC_2', 'CC_3', 'CC_4', 'CC_5', 'CC_6', 'CC_7', 'CG_left', 'CG_right', 'CST_left', 'CST_right', 'FPT_left', 'FPT_right', 'ICP_left', 'ICP_right', 'IFO_left', 'IFO_right', 'ILF_left', 'ILF_right', 'MCP', 'MLF_left', 'MLF_right', 'OR_left', 'OR_right', 'POPT_left', 'POPT_right', 'SCP_left', 'SCP_right', 'SLF_III_left', 'SLF_III_right', 'SLF_II_left', 'SLF_II_right', 'SLF_I_left', 'SLF_I_right', 'STR_left', 'STR_right', 'ST_FO_left', 'ST_FO_right', 'ST_OCC_left', 'ST_OCC_right', 'ST_PAR_left', 'ST_PAR_right', 'ST_POSTC_left', 'ST_POSTC_right', 'ST_PREC_left', 'ST_PREC_right', 'ST_PREF_left', 'ST_PREF_right', 'ST_PREM_left', 'ST_PREM_right', 'T_OCC_left', 'T_OCC_right', 'T_PAR_left', 'T_PAR_right', 'T_POSTC_left', 'T_POSTC_right', 'T_PREC_left', 'T_PREC_right', 'T_PREF_left', 'T_PREF_right', 'T_PREM_left', 'T_PREM_right', 'UF_left', 'UF_right']
 print(Track_name)
 
@@ -36,7 +33,7 @@ for track in Track_name:
     Ima1.sort()
     name_Ima1 = [i.split("/")[-1].split(track)[0] for i in Ima1 ]
     print(name_Ima1)
-    ima_index = nib.load(glob(rep+ "stat/" + track+"*TDI*Thr80.nii")[0]).get_data()
+    ima_index = nib.load(glob("stat/" + track+"*TDI*Thr80.nii")[0]).get_data()
     ind_mean = np.where(ima_index)
     Data1 = []
     for i in Ima1:
