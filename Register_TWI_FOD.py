db8d55369c936c1c011274c879c6b6829f703d93
Renard <felixrenard@gmail.com>
2020-12-01 20:43:40 +0100

    Update Register_TWI_FOD.py
    modify TWI_Gaussian by TW_FOD_Gaussian; TWI2_FA by TW_FA; TWI_FA by Fractional

--- Register_TWI_FOD.py
+++ Register_TWI_FOD.py
@@ -24,15 +24,15 @@ for name in file_name[s*10:(s+1)*10]:
 	print(tmp_track)
 	for track in tmp_track:
 		track_tmp = track.split(".")[0]
-		cmd = "tckmap "+track+" -stat_tck gaussian -fwhm_tck 8 -template template_FOD.nii.gz -contrast fod_amp -force -image "+name+" -stat_vox mean "+track_tmp+"_TWI_Gaussian.nii.gz"
+		cmd = "tckmap "+track+" -stat_tck gaussian -fwhm_tck 8 -template template_FOD.nii.gz -contrast fod_amp -force -image "+name+" -stat_vox mean "+track_tmp+"_TW_FOD_Gaussian.nii.gz"
 		print(cmd)
 		call(cmd.split(" "))
 		cmd = "tckmap "+track+" "+track_tmp+"_TDI_NoGaussian.nii.gz -template template_FOD.nii.gz -force"
 		call(cmd.split(" "))
-		cmd = "tckmap "+track+" -stat_tck gaussian -fwhm_tck 8 -template template_FOD.nii.gz -contrast scalar_map -image transformed_template/"+tmp+"_FA_template.nii.gz -force -stat_vox mean "+track_tmp+"_TWI2_FA_Gaussian.nii.gz"
+		cmd = "tckmap "+track+" -stat_tck gaussian -fwhm_tck 8 -template template_FOD.nii.gz -contrast scalar_map -image transformed_template/"+tmp+"_FA_template.nii.gz -force -stat_vox mean "+track_tmp+"_TW_FA_Gaussian.nii.gz"
 		print(cmd)
 		call(cmd.split(" "))
-		cmd = "mrcalc -force "+track_tmp+"_TWI_Gaussian.nii.gz 0 -gt transformed_template/"+tmp+"_FA_template.nii.gz -mult "+track_tmp+"_TWI_FA_Gaussian.nii.gz"
+		cmd = "mrcalc -force "+track_tmp+"_TWI_Gaussian.nii.gz 0 -gt transformed_template/"+tmp+"_FA_template.nii.gz -mult "+track_tmp+"_Fractional_Gaussian.nii.gz"
 		print(cmd)
 		call(cmd.split(" "))
 		cmd = "afdconnectivity -afd_map "+tmp+"_"+track_tmp+"_AFD_Gaussian.nii.gz "+name+" "+track
