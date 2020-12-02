955ed8ad02c873253ab3ba32287d4fc81216967b
Renard <felixrenard@gmail.com>
2020-12-02 22:03:45 +0100

    Update Convert_mask_percentile_phase2.py
    Modify ROI_name notation
    Add exception if the ROI doesn't exist

--- Convert_mask_percentile_phase2.py
+++ Convert_mask_percentile_phase2.py
@@ -8,37 +8,15 @@ file_name = glob("stat/*TDI_NoGaussian_Thr80.mif")
 file_name.sort()
 print(file_name)
 
-ROI_name = []
-for name in file_name:
-	tmp = name.split("/")[-1].split("_TDI_NoGauss")[0]
-	ROI_name.append('_'.join(tmp.split('_')[2:]))
-
-ROI_name = np.unique(ROI_name).tolist()
-
-#These exceptions are needed if the prefix name has "_"
-try:
-    ROI_name.remove("")
-except:
-    print("")
-try:    
-    ROI_name.remove("left")
-except:
-    print("")
-try:
-    ROI_name.remove("right")
-except:
-    print("")
-try:
-    for i in range(1,8):
-        ROI_name.remove(str(i))
-except:
-    print("")
-    
+ROI_name = ['AF_left', 'AF_right', 'ATR_left', 'ATR_right', 'CC_1', 'CC_2', 'CC_3', 'CC_4', 'CC_5', 'CC_6', 'CC_7', 'CG_left', 'CG_right', 'CST_left', 'CST_right', 'FPT_left', 'FPT_right', 'ICP_left', 'ICP_right', 'IFO_left', 'IFO_right', 'ILF_left', 'ILF_right', 'MCP', 'MLF_left', 'MLF_right', 'OR_left', 'OR_right', 'POPT_left', 'POPT_right', 'SCP_left', 'SCP_right', 'SLF_III_left', 'SLF_III_right', 'SLF_II_left', 'SLF_II_right', 'SLF_I_left', 'SLF_I_right', 'STR_left', 'STR_right', 'ST_FO_left', 'ST_FO_right', 'ST_OCC_left', 'ST_OCC_right', 'ST_PAR_left', 'ST_PAR_right', 'ST_POSTC_left', 'ST_POSTC_right', 'ST_PREC_left', 'ST_PREC_right', 'ST_PREF_left', 'ST_PREF_right', 'ST_PREM_left', 'ST_PREM_right', 'T_OCC_left', 'T_OCC_right', 'T_PAR_left', 'T_PAR_right', 'T_POSTC_left', 'T_POSTC_right', 'T_PREC_left', 'T_PREC_right', 'T_PREF_left', 'T_PREF_right', 'T_PREM_left', 'T_PREM_right', 'UF_left', 'UF_right']
 print(ROI_name)
 
 for ROI in ROI_name:
-	file_ROIs = glob("stat/*_"+ROI+"*NoGaussian_Thr80.mif")
-	print(file_ROIs)
-	cmd = ["mrmath"]+file_ROIs + ["product",  "stat/" + ROI+"_Mean_TDI_NoGaussian.nii", "-force"]
-	print(cmd)
-	call(cmd)
+	try:
+		file_ROIs = glob("stat/*_"+ROI+"*NoGaussian_Thr80.mif")
+		print(file_ROIs)
+		cmd = ["mrmath"]+file_ROIs + ["product",  "stat/" + ROI+"_Mean_TDI_NoGaussian.nii", "-force"]
+		print(cmd)
+		call(cmd)
+	except:
+		print("Cannot proceed the ROI "+ROI)
